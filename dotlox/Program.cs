13100f7f9aa59c0fb76180f3c92cd036110bd584
Nisar Ahmad <nisarahmad1324@gmail.com>
2021-12-13 04:48:04 +0530

    Add project files.

---
+++ dotlox/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.File;
+using static System.IO.Path;
+
+if (args.Length > 1)
+{
+    WriteLine("Usage: dotlox [script]");
+    System.Environment.Exit(64);
+} 
+else if (args.Length == 1)
+{
+    RunFile(args[0]);
+}
+else
+{
+    RunPrompt();
+}
+
+void RunPrompt()
+{
+    while (true)
+    {
+        Write("> ");
+        var line = ReadLine();
+        if (line == null) break;
+        Run(line);
+    }
+}
+
+void RunFile(string v)
+{
+    var path = Combine(GetCurrentDirectory(), v);
+    Run(ReadAllText(path));
+}
+
+void Run(string v)
+{
+    var scanner = new Scanner(v);
+    List<Token> tokens = scanner.ScanTokens();
+
+    foreach(var token in tokens)
+    {
+        WriteLine(token);
+    }
+}
---
+++ dotlox/Scanner.cs
@@ -0,0 +1,14 @@
+﻿internal class Scanner
+{
+    private string v;
+
+    public Scanner(string v)
+    {
+        this.v = v;
+    }
+
+    internal List<Token> ScanTokens()
+    {
+        throw new NotImplementedException();
+    }
+}
