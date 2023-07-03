61b2f52925de095d002845a023cfde7f1d3be387
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-07-03 20:30:46 +0530

    Add a sample for AstPrinter (Commented out for now)

--- dotlox/Program.cs
+++ dotlox/Program.cs
@@ -11,6 +11,17 @@ internal static class Program
 
     private static void Main(string[] args)
     {
+        // // Ast Printer
+        // var expression = new Expr.Binary(
+        //     new Expr.Unary(
+        //         new Token(TokenType.MINUS, "-", null, 1),
+        //         new Expr.Literal(123)),
+        //     new Token(TokenType.STAR, "*", null, 1),
+        //     new Expr.Grouping(
+        //         new Expr.Literal(45.67)));
+        //
+        // Console.WriteLine(new AstPrinter().Print(expression));
+        
         switch (args.Length)
         {
             case > 1:
